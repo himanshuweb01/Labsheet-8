08e036bbe7cacf30c90492037a5a5a83adae7828
Himanshu Rawat <cu25310035@coeruniversity.ac.in>
2025-11-10 12:09:15 +0530

    Program to check Prime Number

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,23 @@
+#include <iostream>
+using namespace std;
+
 bool isPrime(int n) {
     if (n <= 1) return false;
     for (int i = 2; i <= n/2; i++) {
         if (n % i == 0) return false;
     }
     return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << "Prime Number";
+    else
+        cout << "Not a Prime Number";
+
+    return 0;
 }
